f343155d7412bf0f01f154ecdfcdcd1b1e3cc2e8
Sv443 <contact@sv443.net>
2023-05-26 15:56:37 +0200

    feat: TS

--- dev/numKeysJumpTo.js
+++ dev/numKeysJumpTo.js
@@ -1,23 +1,23 @@
 (() => {
-	const timeSteps = 9; // excluding 0
-    function getX(timeKey, maxWidth) {
-        if(timeKey >= timeSteps)
-            return maxWidth;
-        return Math.floor(maxWidth / timeSteps) * timeKey;
-    }
+  const timeSteps = 9; // excluding 0
+  function getX(timeKey, maxWidth) {
+    if(timeKey >= timeSteps)
+      return maxWidth;
+    return Math.floor(maxWidth / timeSteps) * timeKey;
+  }
 
-	const elem = document.querySelector("#sliderBar");
-	const rect = elem.getBoundingClientRect();
-	const x = getX(1, rect.width);
-	const y = (rect.top + rect.bottom) / 2;
+  const elem = document.querySelector("#sliderBar");
+  const rect = elem.getBoundingClientRect();
+  const x = getX(1, rect.width);
+  const y = (rect.top + rect.bottom) / 2;
 
-	const evt = new MouseEvent("mousedown", {
-        clientX: x,
-		clientY: y,
-		target: elem,
-        bubbles: true,
-        view: window,
-	});
+  const evt = new MouseEvent("mousedown", {
+    clientX: x,
+    clientY: y,
+    target: elem,
+    bubbles: true,
+    view: window,
+  });
 
-    document.body.dispatchEvent(evt);
+  document.body.dispatchEvent(evt);
 })();
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const { exec } = require("child_process");
+const package = require("./package.json");
+
+module.exports = {
+  entry: "./src/BetterYTM.user.ts",
+  mode: "production",
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      use: "ts-loader",
+      exclude: /node_modules/,
+    },
+    {
+      test: /\.html$/i,
+      loader: "html-loader",
+    },
+    {
+      test: /\.s[ac]ss$/i,
+      use: [
+        "style-loader",
+        "css-loader",
+        "sass-loader",
+      ],
+    },
+    ],
+  },
+  plugins: [{
+    apply: (compiler) => {
+      compiler.hooks.afterEmit.tap("AfterEmitPlugin", () => {
+        exec("node ./tools/post-build.js", (_err, stdout, stderr) => {
+          stdout && process.stdout.write(`${stdout}\n`);
+          stderr && process.stderr.write(`${stderr}\n`);
+        });
+      });
+    }
+  }],
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "BetterYTM.user.js",
+    path: path.resolve(__dirname),
+  },
+};
