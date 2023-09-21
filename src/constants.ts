5efa6b09302e936049b53f605d9b2cfc6c8fa661
Sv443 <contact@sv443.net>
2023-09-21 22:29:18 +0200

    ref: comment

--- src/constants.ts
+++ src/constants.ts
@@ -19,5 +19,5 @@ export const scriptInfo = {
   name: GM.info.script.name,
   version: GM.info.script.version,
   namespace: GM.info.script.namespace,
-  buildNumber: "{{BUILD_NUMBER}}" as string, // assert as generic string instead of literal
+  buildNumber: "{{BUILD_NUMBER}}" as string, // asserted as generic string instead of literal
 };
