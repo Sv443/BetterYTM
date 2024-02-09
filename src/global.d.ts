aa1ab39774e4dd28af736eace769e2f6a45ba3eb
Sven <s.fehler@pbsnetwork.eu>
2024-02-10 00:09:09 +0100

    fix: separate globals and declare module blocks

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,18 +1,11 @@
-// TODO: verify
-/** Import HTML as modules - https://stackoverflow.com/a/47705264/3323672 */
-declare module "*.html" {
-  /** Content of the HTML file as a string */
-  const htmlContent: string;
-  export default htmlContent;
-}
+import type { mountDom, h } from "@gera2ld/jsx-dom";
 
-declare module "*.md" {
-  interface Exports {
-    /** Content of the markdown file, converted to an HTML string */
-    html: string;
-    metadata: Record<string, unknown>;
-    filename: string;
-    path: string;
-  }
-  export default {} as Exports;
+declare global {
+  /** from @violentmonkey/dom */
+  declare const VM: {
+    /** Mounts a JSX element into a DOM element */
+    m: typeof mountDom;
+    /** Like React.createElement */
+    h: typeof h;
+  };
 }
