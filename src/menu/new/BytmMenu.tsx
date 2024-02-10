f7e4ce650db49afbe35136f342d314d51c0dc945
Sv443 <contact@sv443.net>
2024-02-10 15:56:27 +0100

    feat: more BytmMenu stuff

--- src/menu/new/BytmMenu.tsx
+++ src/menu/new/BytmMenu.tsx
@@ -1,6 +1,7 @@
-import * as ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import * as React from "react";
-import { clearInner, NanoEmitter, warn } from "../../utils";
+import { clearInner, getResourceUrl, NanoEmitter, warn } from "../../utils";
+import { t } from "../../translations";
 
 export interface BytmMenuConfig {
   /** ID that gets added to all child element IDs and class names */
@@ -39,16 +40,27 @@ export class BytmMenu extends NanoEmitter<BytmMenuEventsMap> {
   }
 
   /** Call after DOMContentLoaded to pre-render the menu (or call just before calling open()) */
-  public render() {
+  public async render() {
     const bgElem = document.createElement("div");
     bgElem.id = `bytm-${this.id}-menu-bg`;
     bgElem.classList.add("bytm-menu-bg");
+    bgElem.title = t("close_menu_tooltip");
     bgElem.style.visibility = "hidden";
     bgElem.style.display = "none";
 
+    bgElem.addEventListener("click", (e) => {
+      if(this.isOpen && (e.target as HTMLElement)?.id === `bytm-${this.id}-menu-bg`)
+        this.close(e);
+    });
+    document.body.addEventListener("keydown", (e) => {
+      if(this.isOpen && e.key === "Escape")
+        this.close(e);
+    });
+
     document.body.appendChild(bgElem);
 
-    ReactDOM.render(<MenuContent></MenuContent>, bgElem);
+    const root = createRoot(bgElem);
+    root.render(await this.getMenuContent());
 
     this.events.emit("render");
   }
@@ -56,7 +68,7 @@ export class BytmMenu extends NanoEmitter<BytmMenuEventsMap> {
   /** Clears all menu contents in preparation for a new rendering call */
   public clear() {
     const clearSelectors = [
-      `bytm-${this.id}-menu-bg`,
+      `#bytm-${this.id}-menu-bg`,
     ];
 
     for(const selector of clearSelectors) {
@@ -65,6 +77,8 @@ export class BytmMenu extends NanoEmitter<BytmMenuEventsMap> {
         continue;
       clearInner(elem);
     }
+
+    document.querySelector(`#bytm-${this.id}-menu-bg`)?.remove();
 
     this.events.emit("clear");
   }
@@ -110,15 +124,34 @@ export class BytmMenu extends NanoEmitter<BytmMenuEventsMap> {
 
     this.events.emit("close");
   }
-}
 
-function MenuContent() {
-  // TODO:
-  return (
-    <div className="bytm-menu-content">
-      <div className="bytm-menu-header" id="bytm-menu-header"></div>
-      <div className="bytm-menu-body" id="bytm-menu-body"></div>
-      <div className="bytm-menu-footer" id="bytm-menu-footer"></div>
-    </div>
-  );
+  private async getMenuContent() {
+    const closeSrc = await getResourceUrl("img-close");
+
+    const header = this.config.renderHeader?.();
+    const footer = this.config.renderFooter?.();
+
+    // TODO:
+    return (
+      <div id={`bytm-${this.id}-menu`} className="bytm-menu" title="">
+        <div id="kahlknkpnaf"></div>
+        <div className="bytm-menu-header">
+          {header ? (
+            <div className="bytm-menu-title-wrapper" role="heading" aria-level={1}>
+              {header}
+            </div>
+          ) : null}
+          <img className="bytm-menu-close" src={closeSrc} role="button" />
+        </div>
+        <div>
+          {this.config.renderBody()}
+        </div>
+        {footer ? (
+          <div>
+            {footer}
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
