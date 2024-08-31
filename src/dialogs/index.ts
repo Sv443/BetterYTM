189c2007f568051659e30ceede5bdfc247063afa
Sv443 <contact@sv443.net>
2024-08-31 21:39:09 +0200

    feat: generic prompt dialog

--- src/dialogs/index.ts
+++ src/dialogs/index.ts
@@ -4,5 +4,6 @@ export * from "./autoLike.js";
 export * from "./changelog.js";
 export * from "./featConfig.js";
 export * from "./featHelp.js";
+export * from "./prompt.js";
 export * from "./versionNotif.js";
 export * from "./welcome.js";
