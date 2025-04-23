50314c737073e8c5eb0c0777a0d00a864a518b46
Sv443 <contact@sv443.net>
2025-04-23 20:23:33 +0200

    chore: build dev

--- dist/BetterYTM.user.js
+++ dist/BetterYTM.user.js
@@ -8,7 +8,7 @@
 // @license           AGPL-3.0-only
 // @author            Sv443
 // @copyright         Sv443 (https://github.com/Sv443)
-// @icon              https://cdn.jsdelivr.net/gh/Sv443/BetterYTM@85d8f880/assets/images/logo/logo_dev_48.png
+// @icon              https://cdn.jsdelivr.net/gh/Sv443/BetterYTM@270f5958/assets/images/logo/logo_dev_48.png
 // @match             https://music.youtube.com/*
 // @match             https://www.youtube.com/*
 // @run-at            document-start
@@ -336,7 +336,7 @@ const rawConsts = {
     mode: "development",
     branch: "develop",
     host: "github",
-    buildNumber: "85d8f880",
+    buildNumber: "270f5958",
     assetSource: "jsdelivr",
     devServerPort: "8710",
 };
@@ -4414,6 +4414,11 @@ async function openCfgMenu() {
         menuBg.style.visibility = "visible";
         menuBg.style.display = "block";
         checkToggleScrollIndicator();
+        const kbdElems = menuBg.querySelectorAll("kbd");
+        for (const kbdElem of kbdElems) {
+            kbdElem.classList.add("bytm-kbd");
+            kbdElem.addEventListener("selectstart", (e) => e.preventDefault());
+        }
     }
     catch (err) {
         error("Error while opening config menu:", err);
