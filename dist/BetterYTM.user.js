ee2d00462b74ca42a696e35bf8f301eab52383ec
Sv443 <contact@sv443.net>
2026-03-08 23:27:15 +0100

    chore: build dev

--- dist/BetterYTM.user.js
+++ dist/BetterYTM.user.js
@@ -7,7 +7,7 @@
 // @license           AGPL-3.0-or-later
 // @author            Sv443
 // @copyright         Sv443 (https://github.com/Sv443)
-// @icon              https://cdn.jsdelivr.net/gh/Sv443/BetterYTM@4a0cdae3/assets/images/logo/logo_dev_48.png
+// @icon              https://cdn.jsdelivr.net/gh/Sv443/BetterYTM@e453e6ff/assets/images/logo/logo_dev_48.png
 // @match             https://music.youtube.com/*
 // @match             https://www.youtube.com/*
 // @run-at            document-start
@@ -441,8 +441,8 @@ const rawConsts = {
     mode: "development",
     branch: "develop",
     host: "github",
-    buildNumber: "4a0cdae3",
-    buildTimestamp: "1772993436469",
+    buildNumber: "e453e6ff",
+    buildTimestamp: "1773008833042",
     assetSource: "jsdelivr",
     devServerPort: "8710",
 };
