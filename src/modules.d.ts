19f4b07086aa3e575abdc44d6ca80d045353f07f
Sv443 <contact@sv443.net>
2026-03-10 16:31:10 +0100

    ref: replace greasemonkey with tampermonkey types

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,4 +1,4 @@
-/// <reference types="greasemonkey" />
+/// <reference types="tampermonkey" />
 
 /** Import HTML as modules - https://stackoverflow.com/a/47705264/3323672 */
 declare module "*.html" {
